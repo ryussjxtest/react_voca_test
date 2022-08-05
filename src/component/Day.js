37d1386122f6d78b996f52de65fedf6da7f3969a
ryussjxtest <ryussjxtest@gmail.com>
2022-08-05 21:31:40 +0900

    uesEffect complete

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,4 +1,5 @@
-import dummy from "../db/data.json";
+// import dummy from "../db/data.json";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import DayList from './DayList';
 import Word from "./Word";
@@ -7,31 +8,31 @@ import Word from "./Word";
 export default function Day(){
   // const {day} = useParams();
   const day = Number(useParams().day);
-  const wordList = dummy.words.filter(word=>word.day === day)
-  console.log(day,wordList);
+  // const wordList = dummy.words.filter(word=>word.day === day)
+  // console.log(day,wordList);
 
+  const [words, setWords] = useState([]);
+
+  useEffect(()=>{
+    // fetch("http://localhost:3001/words?day=1")
+    fetch(`http://localhost:3001/words?day=${day}`)
+    .then(res=>{
+      return res.json();
+    })
+    .then(data=>{
+      setWords(data);
+    })
+  },[day]);
   return (
     <>
       <DayList />
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word=>(
+          {words.map(word=>(
             // Word로 이동.. 뜻보기 버튼으로 뜻을 보이고 안보이게 하려면...
             // 별도의 컴포넌트로 각 단어별 state가 있어야 한다.
             <Word word={word} key={word.id}/>
-            // <tr key={word.id}>
-            //   <td>
-            //     <input type='checkbox' />
-            //   </td>
-            //   <td>{word.eng}</td>
-            //   <td>{word.kor}</td>
-            //   <td>
-            //     <button>뜻보기</button>
-            //     <button className="btn_del">삭제</button>
-            //   </td>
-            //   <td>{word.isDone? "암기":"암기필요"}</td>
-            // </tr>
           ))}
         </tbody>
       </table>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -6,35 +6,24 @@ import { Link } from 'react-router-dom';
 export default function DayList(){
   // console.log(dummy)
   const[days, setDays] = useState([]);
-  const[count, setCount] = useState(0);
-
-  function onClick(){
-    setCount(count + 1);
-  }
-
-  function onClick2(){
-    setDays(
-      [...days,
-        {
-          id:Math.random(),
-          day:1,
-        }
-      ]
-    );
-  }
+  
+  
   // useEffect는 어떤 상태 값이 변경되었을때 효과를 주기 위한 함수 이다.
   // function useEffect(effect: EffectCallback, deps?: DependencyList): void;
   // DayList page가 변경되면 호출된다.
   // 하지만 관심없는 항목이 수정되더라도 항상 호출이 되므로..두번째 인자로 관심있는 항목..
   // 즉 변경되면 알고 싶은 것들을 list형태로 넣어 준다.
   useEffect(()=>{
-    //effect callback fn.
-    console.log('[useEffect] Count changed..', count);
-  },[count]);
-  // 의존성 배열.
-  // 의존성 배열이 변경된 경우에만 useEffect가 동작하고..그래서 log가 찍힌다.
-  // 최초에 한번만 실행... 렌더링 되고 api 호출.
-  // 렌더링 완료되고 한번만   -> 의존성 배열에 빈배열...[]
+    fetch('http://localhost:3001/days') // json-server로 만든것...
+    .then(res=>{
+      return res.json();
+    })
+    .then(data=> {
+      setDays(data);
+    })
+  },[]); // 의존성 배열 null []. page렌더링이후, 최초 한번만 실행
+  // json-server --watch ./src/db/data.json --port 3001
+  // json-server로 backend 실행해 놓아야 한다.
 
   return (
     <>
@@ -47,9 +36,6 @@ export default function DayList(){
           </li>   
         ))}
       </ul>
-      {/* <button onClick={()=>{setCount(count + 1);}}>{count}</button> */}
-      <button onClick={onClick}>{count}</button>
-      <button onClick={onClick2}>day change</button>
     </>
     
   );
