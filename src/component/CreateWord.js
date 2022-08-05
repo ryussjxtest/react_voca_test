1722417edd92bab35bd2d0b124115e4365c52d81
ryussjxtest <ryussjxtest@gmail.com>
2022-08-06 04:42:13 +0900

    저장중 버튼 막기

--- src/component/CreateWord.js
+++ src/component/CreateWord.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useHistory } from "react-router-dom";
 import useFetch from "../hook/useFetch";
 
@@ -9,6 +9,11 @@ export default function CreateWord(){
   // 추가 하면 바로 추가된 day로 이동하도록 한다.
   // 여기에서 react-router-dom가 제공하는 useHistory를 이용한다.
   const history = useHistory(); // 선언하고 아래서 push로 이동할 url넣는다.
+
+  //네트웍을 느리게 한다음(broswer>F12>Netwrok>Online(No throttling)-> slow3G)
+  // 단어 추가 버튼을 빠르게 누르면...하나의 단어가 추가 되기 전에 지속 입력된다.
+  // 하나의 단어 추가가 끝나기 전(res 200 OK )에는 단어 추가가 되지 않도록 한다.
+  const [isSaving, setIsSaving] =  useState(false);
 
   // 버튼을 눌러도 새로 하지 않도록 막는다.
   function onSubmit(e){
@@ -21,26 +26,32 @@ export default function CreateWord(){
     // console.log('[onSubmit korRef] ',newKor);
     // console.log('[onSubmit dayRef] ',newDay);
 
-    fetch(`http://localhost:3001/words`,{
-      method : "POST",
-      headers:{
-        "Content-Type" : "application/json",
-      },
-      body:JSON.stringify({
-        // id:
-        day: newDay,
-        eng: newEng,
-        kor: newKor,
-        isDone : false
-      }),
-    }).then(res=>{
-      if(res.ok){
-        // alert("생성이 완료 되었습니다.")
-        console.log("POST OK", newDay, newEng, newKor);
-        // link to 처럼 페이지 이동, link to , a tag없이 page이동가능.
-        history.push(`/day/${newDay}`);  
-      }
-    })
+    if(!isSaving){
+      setIsSaving(true);
+
+      fetch(`http://localhost:3001/words`,{
+        method : "POST",
+        headers:{
+          "Content-Type" : "application/json",
+        },
+        body:JSON.stringify({
+          // id:
+          day: newDay,
+          eng: newEng,
+          kor: newKor,
+          isDone : false
+        }),
+      }).then(res=>{
+        if(res.ok){
+          alert("생성이 완료 되었습니다.")
+          console.log("POST OK", newDay, newEng, newKor);
+          setIsSaving(false);
+
+          // link to 처럼 페이지 이동, link to , a tag없이 page이동가능.
+          history.push(`/day/${newDay}`); 
+        }
+      })
+    }
   }
 
   // form 내부에서 선택한 값을 읽어 오기 위한 부분.
@@ -79,7 +90,10 @@ export default function CreateWord(){
       {/* <button className="btn_del">저장</button> */}
       <button onClick={()=>{
           console.log("[CreateWord] onClick.........")
-        }}>저장</button>
+        }}
+        style={{opacity : isSaving? 0.3 : 1}}>
+          {isSaving? "Saving..": "저장"}
+        </button>
     </form>
   )
 }
